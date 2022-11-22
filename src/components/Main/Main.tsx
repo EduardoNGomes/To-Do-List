9b0c78089aa1a470d418647e4e05d575fc3dcdbb
Eduardo N Gomes <Eduardo.000@live.com>
2022-11-22 19:05:52 -0300

    Create feature changeTask And Remove Task

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,16 +3,8 @@ import style from './Main.module.css'
 import { NewTaskBox } from '../NewTaskBox/NewTaskBox'
 import { TaskBox } from '../TaskBox/TaskBox'
 
-import { useState } from 'react'
-import {UUID} from 'uuid-generator-ts';
+import { useEffect, useState } from 'react'
 import {v4 as uuidv4} from 'uuid';
-
-
-// const task = {
-//   id: 5,
-//   done: true,
-//   content:'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.'
-// }
 
 
 interface TaskProps{
@@ -22,9 +14,16 @@ interface TaskProps{
 
 }
 
-
 export const Main = () => {
-  const [allTask, setAllTask] = useState<TaskProps[]>([])
+  const [allTask, setAllTask] = useState<TaskProps[]>(() => {
+    const data = localStorage.getItem('@to-do_list')
+    if(data){
+      return JSON.parse(data)
+    } else {
+      return []
+    }
+  })
+  const [taskDone, setTaskDone] = useState(0)
   
 
   const createNewTask = (value:string) => {
@@ -34,8 +33,34 @@ export const Main = () => {
       content:value
     }
     setAllTask(prevState => [...prevState, task])
-    console.log(allTask)
   }
+
+  const removeTask = (id:string) => {
+    const filteredTask = allTask.filter(task => task.id !== id)
+
+    setAllTask(filteredTask)
+  }
+
+  const changeTask = (id:string) => {
+    const taskChanged = allTask.filter(task => {
+      if(task.id == id){
+        task.done= !task.done
+      }
+      return task
+    })
+
+    setAllTask(taskChanged)
+  }
+
+  useEffect(()=>{
+    const task = allTask.filter(task => task.done ===true)
+    setTaskDone(task.length)
+  },[allTask])
+
+
+  useEffect(()=>{
+    localStorage.setItem('@to-do_list', JSON.stringify(allTask))
+  },[allTask])
 
   return (
     <main className={style.main}>
@@ -45,8 +70,8 @@ export const Main = () => {
 
       <div className={style.content}>
         <div className={style.header}>
-          <p>Tarefas criadas <span>5</span> </p>
-          <p>Concluidas <span>2 de 5</span> </p>
+          <p>Tarefas criadas <span>{allTask.length}</span> </p>
+          <p>Concluidas <span>{taskDone} de {allTask.length}</span> </p>
         </div>
 
         {allTask &&
@@ -54,8 +79,11 @@ export const Main = () => {
           allTask.map(task => 
             <TaskBox
               key={task.id}
+              id={task.id}
               done={task.done}
               content={task.content}
+              remove={removeTask}
+              change={changeTask}
             />
           )
         }
--- src/components/TaskBox/TaskBox.tsx
+++ src/components/TaskBox/TaskBox.tsx
@@ -3,14 +3,28 @@ import { BiCircle, BiTrash } from 'react-icons/bi'
 import {AiFillCheckCircle} from 'react-icons/ai'
 
 interface TaskBoxProps {
+  id:string
   done:boolean;
   content: string;
+  remove:(id:string)=> void
+  change:(id:string)=> void
 }
 
-export const TaskBox = ({done, content}:TaskBoxProps) => {
+export const TaskBox = ({id,done, content, remove, change}:TaskBoxProps) => {
+
+  const handleRemove = () =>{
+    remove(id)
+  }
+
+  const handleChange = () => {
+    change(id)
+  }
   return(
     <article className={style.article}>
-      <button className={done ? style.active: ''}>
+      <button 
+        className={done ? style.active: ''}
+        onClick={handleChange}
+      >
         {done ? 
           <AiFillCheckCircle size={18}/> 
           : 
@@ -24,7 +38,7 @@ export const TaskBox = ({done, content}:TaskBoxProps) => {
       </p>
 
 
-      <button>
+      <button onClick={handleRemove}>
         <BiTrash size={16}/>
       </button>
 
